9e5f56e0eefa61c4af009f5e0a335d8087fbc748
Emmanuel Tom Jose <84658038+leantos@users.noreply.github.com>
2024-11-26 17:49:27 +0530

    Before adding google fonts, gradle working

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/questions.dart';
+import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -22,7 +22,7 @@ class _QuizState extends State<Quiz> {
 
   void switchScreen() {
     setState(() {
-      activeScreen = const Questions();
+      activeScreen = const QuestionsScreen();
     });
   }
 
